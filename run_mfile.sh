034ae3e378c4e2c4ea19583933972387c6711633
Thiago Gouvêa <tsgouvea@gmail.com>
2018-07-12 17:47:38 -0400

    dk

--- run_mfile.sh
+++ run_mfile.sh
@@ -2,15 +2,15 @@
 
 if ( command -v matlab &> /dev/null )
 then
-  cmd='matlab -nosplash -nojvm -r'
+  echo Matlabing
+  matlab -nosplash -nojvm -r "try, cd(""'"$(dirname $0)"'""), $1, exit(0), catch err, disp(getReport(err,'extended')), exit(-1), end" | tail -n +13
 else
-  if ( command -v octave &> /dev/null )
+  if ( command -v octavez &> /dev/null )
   then
-    cmd='octave --no-gui --eval'
+    echo Octaving
+    octave --no-gui --eval "cd(""'"$(dirname $0)"'""), $1, exit(0);"
   else
     printf "\nCould not find Matlab nor Octave.\n\tMake sure at least one is installed and callable.\n\tError at $0\n\tCalled with arguments: $@\n\n"
     exit 1
   fi
 fi
-$cmd "cd(""'"$(dirname $0)"'), $1"
-exit 0
